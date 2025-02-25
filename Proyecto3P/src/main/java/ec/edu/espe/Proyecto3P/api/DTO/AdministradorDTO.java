1e1921d96ea40517df427741ce16d701a7e307e2
Freddy Sebastián Rodríguez Alvear <107556691+10038-fsrodriguez1@users.noreply.github.com>
2025-02-25 11:50:41 -0500

    feat: AdministradorDTO

--- Proyecto3P/src/main/java/ec/edu/espe/Proyecto3P/api/DTO/AdministradorDTO.java
+++ Proyecto3P/src/main/java/ec/edu/espe/Proyecto3P/api/DTO/AdministradorDTO.java
@@ -1,4 +1,15 @@
 package ec.edu.espe.Proyecto3P.api.DTO;
 
-public class AdministradorDTO {
+
+
+public class AdministradorDTO extends UsuarioDTO {
+
+    // Constructor vacío
+    public AdministradorDTO() {
+    }
+
+    // Constructor con parámetros
+    public AdministradorDTO(Long id, String username, String email, boolean activo) {
+        super(id, username, email, activo);
+    }
 }
