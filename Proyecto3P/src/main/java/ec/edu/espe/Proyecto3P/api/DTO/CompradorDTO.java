5908c74df4876a21a6d16e9abd7520d57ab185f1
Freddy Sebastián Rodríguez Alvear <107556691+10038-fsrodriguez1@users.noreply.github.com>
2025-02-25 11:53:27 -0500

    feat: CompradorDTO
    CompradorDTO

--- Proyecto3P/src/main/java/ec/edu/espe/Proyecto3P/api/DTO/CompradorDTO.java
+++ Proyecto3P/src/main/java/ec/edu/espe/Proyecto3P/api/DTO/CompradorDTO.java
@@ -1,4 +1,27 @@
 package ec.edu.espe.Proyecto3P.api.DTO;
 
-public class CompradorDTO {
+
+import java.util.List;
+
+public class CompradorDTO extends UsuarioDTO {
+    private List<PujaDTO> pujasRealizadas;
+
+    // Constructor vacío
+    public CompradorDTO() {
+    }
+
+    // Constructor con parámetros
+    public CompradorDTO(Long id, String username, String email, boolean activo, List<PujaDTO> pujasRealizadas) {
+        super(id, username, email, activo);
+        this.pujasRealizadas = pujasRealizadas;
+    }
+
+    // Getters y Setters
+    public List<PujaDTO> getPujasRealizadas() {
+        return pujasRealizadas;
+    }
+
+    public void setPujasRealizadas(List<PujaDTO> pujasRealizadas) {
+        this.pujasRealizadas = pujasRealizadas;
+    }
 }
