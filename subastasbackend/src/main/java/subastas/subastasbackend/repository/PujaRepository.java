2a4f143587607c0353f8575523873901be9702fa
Lizeth Magaly Pillajo Andrango <86936946+LMagaly@users.noreply.github.com>
2025-03-04 21:56:13 -0500

    Repository:Puja

---
+++ subastasbackend/src/main/java/subastas/subastasbackend/repository/PujaRepository.java
@@ -0,0 +1,8 @@
+package subastas.subastasbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import subastas.subastasbackend.model.Puja;
+
+public interface PujaRepository extends JpaRepository<Puja, Integer> {
+
+}
